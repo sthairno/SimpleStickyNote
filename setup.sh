44b1758f3de78531952a8265a821f802314f6cf0
sthairno <45668272+sthairno@users.noreply.github.com>
2021-03-26 10:21:35 +0000

    画像のダウンロードスクリプトを追加

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+
+mkdir public/img
+
+wget "https://free-paper-texture.com/p/p0435/p0435_s.png" -O public/img/paper.png
+wget "https://free-paper-texture.com/p/p0141/p0141_s.png" -O public/img/cork.png
