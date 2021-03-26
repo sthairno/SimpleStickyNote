e89ef7746231e395d44680cd09ee98630ce1720b
sthairno <45668272+sthairno@users.noreply.github.com>
2021-03-26 17:49:41 +0000

    ログイン機能を実装

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,13 +1,24 @@
 import $ from "jquery";
+import { Modal } from "bootstrap";
+
+//Firebase Firestore
 import firebase from "firebase/app";
 import "firebase/firestore"
+//Firebase UI
+import * as firebaseui from 'firebaseui'
 
 import { StickyNote, stickyNoteGenerator } from "./stickynote";
 
 export let stickyNotes: StickyNote[] = [];
+
 let stickynoteArea: HTMLElement;
+let authModal: Modal;
 $(function () {
-    stickynoteArea = <HTMLElement>document.getElementById('stickynote-area');
+    stickynoteArea = <HTMLElement>document.getElementById("stickynote-area");
+    authModal = new Modal(document.getElementById("auth-modal")!, {
+        backdrop: "static",
+        keyboard: false
+    });
 });
 
 var firebaseConfig = {
@@ -19,11 +30,43 @@ var firebaseConfig = {
     appId: "1:966043568574:web:75cd1b7df71697543c51ef",
     measurementId: "G-QL3XLDJPDG"
 };
+var uiConfig: firebaseui.auth.Config = {
+    signInOptions: [
+      // Leave the lines as is for the providers you want to offer your users.
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+        signInSuccessWithAuthResult: (result, redirectUrl) => {
+            return false; //リダイレクトしない
+        }
+    },
+    signInFlow: "popup"
+};
 
 let firestore: firebase.firestore.Firestore;
 
 $(function() {
     firebase.initializeApp(firebaseConfig);
+
+    let ui = new firebaseui.auth.AuthUI(firebase.auth());
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            console.log("Auth state changed: Signed In");
+            authModal.hide();
+            $("#user-btn").text(user.displayName ? user.displayName : "User");
+        } else {
+            console.log("Auth state changed: Signed Out");
+            ui.start('#firebaseui-auth-container', uiConfig);
+            authModal.show();
+        }
+    });
+    
+    $("#logout-btn").on("click", (e)=>{
+        firebase.auth().signOut();
+    });
+
     firestore = firebase.firestore();
 })
 
